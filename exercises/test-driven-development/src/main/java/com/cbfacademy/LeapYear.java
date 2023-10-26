f5d04b96777f9e2460213e7b031435d8bac76000
Esther Makinde <esthermakinde@hotmail.com>
2023-10-26 21:13:13 +0100

    Refactored LeapYear class using ternary operator

--- exercises/test-driven-development/src/main/java/com/cbfacademy/LeapYear.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/LeapYear.java
@@ -7,19 +7,11 @@ public class LeapYear {
         this.year = year;
     }
 
-    public Boolean isLeapYear() {
-        if (this.year % 400 == 0) {
-            return true;
-        } else if (this.year % 100 == 0 && this.year % 400 != 0) {
-            return false;
-        } else if (this.year %4 == 0 && this.year % 100 !=0) {
-            return true;
-        } else if (this.year %4 != 0) {
-            return false;
-        }
-        return false;
-    }
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+      }
 }
+
 /*
  * if(years % 400 == 0) -> true
  * years % 100 == 0 && years % 400 != 0 -> false
